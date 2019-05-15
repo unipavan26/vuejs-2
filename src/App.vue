c9ff4f318a9723860a87b034dc3edbfdcdc5b2b6
PNS <unipavan26@gmail.com>
2019-05-15 16:01:45 +0530

    calci

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <calculator/>
+  </div>
+</template>
+
+<script>
+import calculator from './components/calculator.vue'
+
+export default {
+  name: 'app',
+  data () {
+    
+  },
+
+  components:{
+    calculator
+  }
+}
+</script>
+
+<style>
+</style>
